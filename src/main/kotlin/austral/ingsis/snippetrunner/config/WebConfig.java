81ef3c43b36c46a683e0d045d11665103dd10bc4
tomasfreile <freiletomy@gmail.com>
2024-06-19 18:04:32 -0300

    add cors config

---
+++ src/main/kotlin/austral/ingsis/snippetrunner/config/WebConfig.java
@@ -0,0 +1,25 @@
+package austral.ingsis.snippetrunner.config;
+
+import org.jetbrains.annotations.NotNull;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class WebConfig implements WebMvcConfigurer {
+
+    @Bean
+    public WebMvcConfigurer corsConfigurer() {
+        return new WebMvcConfigurer() {
+            @Override
+            public void addCorsMappings(@NotNull CorsRegistry registry) {
+                registry.addMapping("/**")
+                        .allowedOrigins("http://localhost:5173", "http://printscript-ui:5173", "http://localhost:8081", "http://snippet-operations:8081")
+                        .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
+                        .allowedHeaders("*")
+                        .allowCredentials(true);
+            }
+        };
+    }
+}
